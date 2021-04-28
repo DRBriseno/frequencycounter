1b19c194ee004a726aacc55716d7347c53d11c2c
Rose <71735227+DRBriseno@users.noreply.github.com>
2021-04-27 23:26:33 -0700

    Add files via upload
    Updated HashTable.py with insert method completed. Having difficulty pushing, so I uploaded.

--- HashTable.py
+++ HashTable.py
@@ -24,8 +24,12 @@ class HashTable:
   # Hash functions are a function that turns each of these keys into an index value that we can use to decide where in our list each key:value pair should be stored. 
 
   def hash_func(self, key):
-    
-    pass
+    w_length = len(key)
+    w_length = ord(key[-1])
+    index = w_length % self.size
+
+    return index
+
 
 
   # 3️⃣ TODO: Complete the insert method.
@@ -33,9 +37,19 @@ class HashTable:
   # Should insert a key value pair into the hash table, where the key is the word and the value is a counter for the number of times the word appeared. When inserting a new word in the hash table, be sure to check if there is a Node with the same key in the table already.
 
   def insert(self, key, value):
-    pass
-
-
+    updated_data = (key, value)
+    arr_i = self.hash_func(key)
+    arr_i = self.hash_func(updated_data[0])
+    LinkedList = self.arr[arr_i]
+    list_index = LinkedList.find(updated_data[0])
+    print(f'Looking for... {updated_data[0]} in the List[{arr_i}]')
+    if list_index == -1:
+      LinkedList.append(updated_data)
+    else:
+      print(f' A duplicate has been found... #{updated_data[0]}')
+      LinkedList.update_node(key)
+    LinkedList.append(updated_data)
+    return f"{key} was inserted: {value} at index {arr_i}."
 
 
   # 4️⃣ TODO: Complete the print_key_values method.
